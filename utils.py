3d93f393022e41da7d3c5007b0649c3c07e935e4
Donghwan Lee <dh7401@sas.upenn.edu>
2022-02-20 20:42:54 -0500

    Update docstrings

--- utils.py
+++ utils.py
@@ -2,7 +2,13 @@ import numpy as np
 
 def ece(scores, labels, num_bins, p=2, debias=False):
   '''
-  Return lp-ECE_n
+  Input:
+    scores: (Z_1, ... , Z_n) \in [0, 1]^n
+    labels: (Y_1, ..., Y_n) \in {0, 1}^n
+    num_bins: m
+    debias: If True, debias plug-in estimator (only works for p = 2)
+  Output:
+    Plug-in estimator of l_p-ECE(f)^p w.r.t. m equal-width bins
   '''
   indexes = np.floor(num_bins * scores).astype(int)
   counts = np.bincount(indexes)
@@ -17,36 +23,54 @@ def ece(scores, labels, num_bins, p=2, debias=False):
   return error / len(scores)
 
 
-def perturb_scores(scores, num_bins, smoothness, signs, scale):
+def perturb_scores(scores, num_perturbations, smoothness, signs, scale):
   '''
-  Return perturbed scores
+  Input:
+    scores: (Z_1, ... , Z_n) \in [0, 1]^n
+    num_perturbations: m
+    smoothness: Holder exponent s
+    signs: Perturbation signs (a_1, ... , a_n) \in {-1, 1}^n
+    scale: rho
+
+  Output:
+    (g(Z_1), ... , g(Z_n)) where g(Z_i) = Z_i + a_i * rho * m^(-s) * zeta({2 * m * Z_i}) if 0.25 <= Z_i <= 0.75
+                                 g(Z_i) = Z_i otherwise
   '''
-  is_inner = (0.25 <= scores) & (scores < 0.75)
-  bump = lambda x: scale * np.exp(-1 / x / (1 - x))
-  rescale = lambda x: 2 * num_bins * (x - 0.25) - np.floor(2 * num_bins * (x - 0.25))
-  perturbations = is_inner * num_bins**(-smoothness) * bump(rescale(scores)) * signs
+  is_inner = (0.25 <= scores) & (scores <= 0.75)
+  zeta = lambda x: scale * np.exp(-1 / x / (1 - x))
+  rescale = lambda x: 2 * num_perturbations * (x - 0.25) - np.floor(2 * num_perturbations * (x - 0.25))
+  perturbations = is_inner * num_perturbations**(-smoothness) * zeta(rescale(scores)) * signs
   
   return scores + perturbations
 
 
-def chi_squared_statistic(samples1, samples2, num_bins):
+def chi_squared(sample1, sample2, num_bins):
   '''
-  Two-sample chi-squared statistic
+  Input:
+    sample1: (V_1, ... , V_{n_1}) \in [0, 1]^{n_1}
+    sample2: (W_1, ... , W_{n_2}) \in [0, 1]^{n_2}
+    num_bins: m
+  Output:
+    Two-sample chi-squared statistic w.r.t. m equal-width bins
   '''
-  n1 = len(samples1)
-  n2 = len(samples2)
+  n1 = len(sample1)
+  n2 = len(sample2)
+  indexes1 = np.floor(num_bins * sample1).astype(int)
+  indexes2 = np.floor(num_bins * sample2).astype(int)
 
-  indexes1 = np.floor(num_bins * samples1).astype(int)
-  indexes2 = np.floor(num_bins * samples2).astype(int)
+  stat = ((np.bincount(indexes1, minlength=num_bins) / n1 - np.bincount(indexes2, minlength=num_bins) / n2)**2).sum()
 
-  error = ((np.bincount(indexes1, minlength=num_bins) / n1 - np.bincount(indexes2, minlength=num_bins) / n2)**2).sum()
-
-  return error
+  return stat
 
 
 def rejection_sampling(scores, labels):
   '''
-  Perform rejection sampling based on labels/pseudo-labels for the first/second half of given scores
+  Input:
+    scores: (Z_1, ... , Z_n) \in [0, 1]^n
+    labels: (Y_1, ... , Y_n) \in {0, 1}^n
+  Output:
+    Split the scores and perform rejection sampling based on labels/pseudo-labels (see Section 3.3).
+    Output (V_1, ... , V_{n_1}), (W_1, ... , W_{n_2})
   '''
   size = len(scores)
   
@@ -58,3 +82,18 @@ def rejection_sampling(scores, labels):
   idx2 = labels2 == 1
   
   return scores1[idx1], scores2[idx2]
+
+
+def consistency_resampling(scores):
+  '''
+  Input:
+    scores: (Z_1, ... , Z_n) \in [0, 1]^n
+  Output:
+    Sample (Z_1', ... , Z_n') with replacement from {Z_1, ... , Z_n}
+    Output (Y_1', ... , Y_n') where Y_i' ~ Ber(Z_i')
+  '''
+  n = len(scores)
+  sampled_scores = np.random.choice(scores, size=n, replace=True)
+  sampled_labels = np.random.binomial(1, sampled_scores, n)
+
+  return sampled_scores, sampled_labels
